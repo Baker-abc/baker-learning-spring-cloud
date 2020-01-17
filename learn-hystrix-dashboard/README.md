0becb71389915435ad4c88641935c7fb096bf70e
Baker-abc <1823023764@qq.com>
2020-01-17 09:06:14 +0800

    hystrix dashboard

---
+++ learn-hystrix-dashboard/README.md
@@ -0,0 +1,12 @@
+
+
+http://localhost:5009/hystrix 
+
+
+
+	http://localhost:5007/actuator/hystrix.stream	可以查看Dashboard
+	在上⾯的输⼊框填⼊:	http://想监控的服务:端⼝/hystrix.stream进
+⾏测试
+	注意：⾸先要先调⽤⼀下想监控的服务的API，否则将会显示⼀个空的图表.
+
+	
---
+++ learn-hystrix-dashboard/src/test/java/com/baker/learning/learnhystrixdashboard/LearnHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baker.learning.learnhystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnHystrixDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
